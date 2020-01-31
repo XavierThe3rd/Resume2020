7377cf38139db3a36f3440121e590034634f63f6
Anthony Grasso <anthonygrasso@Anthonys-MBP-2.dhcp.kimtom.net>
2020-01-31 15:38:59 -0500

    Azonix Added

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = ()=> {
       <Header/>
       <section>
         <ContentBox Background={'linear-gradient(306deg, rgba(97,0,255,1) 0%, rgba(255,38,67,1) 100%)'}>
-          <comps.Title Color={styles.conColor}>{nl2br('Provinding Bold Intuitive \n Design and Programming')}</comps.Title>
+          <comps.Title Color={styles.conColor}>{'Provinding Bold Intuitive Design and Programming'}</comps.Title>
           <p>{'Based in the city of pittsburgh, for the past 2 years I have been aquiring my degree in Graphic Design while also accumulating a vast knowledge of front-end & backend programming skills on my own accord. Now finished I look forward to finally providing my skills to you and your company.'}</p>
           <comps.Button
             Click ={()=>console.log('button was clicked')}
--- src/Apps/Components/portfolioCards/portfolioCards.jsx
+++ src/Apps/Components/portfolioCards/portfolioCards.jsx
@@ -7,21 +7,13 @@ import SmallHamburger from "../Hamburgers/smallHamburger.jsx";
 
 const ProjectCard = ({Image, Title, Project, Type, Color, Desc, Click, Click2})=>{
     const [hoverState, setHover] = useState(false)
-    localStorage.setItem('hover', hoverState)
-    localStorage.setItem('true', setHover)
-    
     const [widthState, setWidth] = useState(false)
     const [buttonState, setButton] = useState(false)
     const [classState, setClass] = useState(false)
 
-    var hover = localStorage.getItem('hover');
-    var t = localStorage.getItem('true');
-
-    console.log(hover)
-
     let project_card_styles = {
         cursor: 'pointer',
-        transition: 'all 0.15s linear',
+        transition: 'all 0.35s ease-in-out',
         //overflow: 'hidden',
     }
 
@@ -42,24 +34,22 @@ const ProjectCard = ({Image, Title, Project, Type, Color, Desc, Click, Click2})=
     }
 
     let title_styles = {
-        marginLeft: 20
+       
     }
 
     let inner_text_styles = {
         background: styles.primePurple,
-        padding: '20px 20px 0',
         zIndex: 1,
-        transition: 'all 0.35s linear'
+        transition: 'all 0.2s linear'
     }
 
     let hover_span_styles = {
-        padding: '0px 0 10px'
+        //padding: '0px 0 10px'
     }
 
     let lower_span_styles = {   
         background: styles.primePurple,
         zIndex: 3,
-        padding: '0 20px 20px',
     }
 
     let toInner = (e)=>{
@@ -78,8 +68,8 @@ const ProjectCard = ({Image, Title, Project, Type, Color, Desc, Click, Click2})=
     return(
       <div className={`project_card column ${classState? `pc-on` : 'pc-off'}`}
         onClick={Click}
-        onMouseEnter={()=>t | setTimeout(()=> setClass(true),10)} 
-        onMouseLeave={()=>setClass(false) |  setTimeout(()=> t,350)} 
+        onMouseEnter={()=>setHover(true) | setTimeout(()=> setClass(true),10)} 
+        onMouseLeave={()=>setClass(false) |  setTimeout(()=> setHover(false),350)} 
         style={project_card_styles}>
         <span className="image_png flx--e abs" style={image_png_styles}>
           <img className="abs" src={Image} alt='none'/>
@@ -96,12 +86,12 @@ const ProjectCard = ({Image, Title, Project, Type, Color, Desc, Click, Click2})=
         </div>
         <h1 style={title_styles}>{Title}</h1>
         <div className="inner_text column" style={inner_text_styles} onClick={Click2}>
-        {hover ? <span className="column" style={hover_span_styles}>
+        {hoverState ? <span className="column" style={hover_span_styles}>
             <p>{Desc}</p>
             <h3>CLICK TO LEARN MORE</h3>
             </span>: null}
         </div>
-        <span className="row-b" style={lower_span_styles}>
+        <span className="lower_span row-b" style={lower_span_styles}>
             <h2 style={{color: Color}}>{Project}</h2><h2 style={{color: Color}}>{Type}</h2>
         </span>
       </div>
